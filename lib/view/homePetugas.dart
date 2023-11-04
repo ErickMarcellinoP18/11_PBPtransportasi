46519fd61bc57dfbda91c7ac19cf2b56a7e508b7
jospun <144347542+jospun@users.noreply.github.com>
2023-11-04 10:22:23 +0700

    homepetugas

---
+++ lib/view/homePetugas.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:transportasi_11/constant/app_constant.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  navigateTo(BuildContext context, String routeName) =>
+      Navigator.pushNamed(context, routeName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(LabelTextConstant.homePageAppBarTitle),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+                onPressed: () =>
+                    navigateTo(context, RouteConstant.routeToQrScanPage),
+                child: const Text(ButtonTextConstant.qrScanning)),
+          ],
+        ),
+      ),
+    );
+  }
+}
